f3775c54ba19433be084298b62c7eb3c25f888a5
Manish Kumar <manish15052000@gmail.com>
2020-11-09 19:41:48 +0530

    up

---
+++ ANDOR.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define for0(i,n)   for ( int (i)=0; (i)<(n); (i)++)
+#define for1(i,n)   for ( int (i)=1; (i)<=(n); (i)++)
+typedef vector<int> vi;
+#define pb push_back
+#define OJ \
+        freopen("input.txt", "r", stdin); \
+        freopen("output.txt", "w", stdout);
+
+int solve(){
+    ll n;
+    cin>>n;
+
+    cout<<0<<" "<<n<<"\n";
+
+
+  return 0;
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  OJ;
+  int t = 1;
+  cin>>t;
+  while(t--) solve();
+}
