a331bf842c7c8374c66d9840aa10d2a91546e3d9
Manish Kumar <manish15052000@gmail.com>
2021-05-18 16:27:41 +0530

    added note

--- Important Notes.md
+++ Important Notes.md
@@ -1,1 +1,9 @@
-> transform(Iterator inputBegin, Iterator inputEnd, Iterator OutputBegin, unary_operation)
+- transform(Iterator inputBegin, Iterator inputEnd, Iterator OutputBegin, unary_operation)
+- Two Pointers
+  > Try to select pointers where moving them handles both cases
+  > Ex: if we take i = 0 and j = n-1 then if i++ and j-- both cases
+  > decrease the difference(A[j] - A[i]), Hence it is good to take
+  > i = 0 & j = 1 so that on i++, difference decreases and on j++
+  > difference increases and then handle the cases.
+  > Follow this in all Two Pointers Questions
+  > (Refer Question Diff - Two Pointers)
--- Two Pointers/Diff.cpp
+++ Two Pointers/Diff.cpp
@@ -4,12 +4,12 @@
 // Space Complexity : O(1)
 // https://www.interviewbit.com/problems/diffk/
 
-// Try to select pointers where moving them handles both cases
-// Ex: if we take i = 0 and j = n-1 then if i++ and j-- both cases
-// decrease the difference(A[j] - A[i]), Hence it is good to take 
-// i = 0 & j = 1 so that on i++, difference decreases and on j++
-// difference increases and then handle the cases.
-// Follow this in all Two Pointers Questions
+Try to select pointers where moving them handles both cases
+Ex: if we take i = 0 and j = n-1 then if i++ and j-- both cases
+decrease the difference(A[j] - A[i]), Hence it is good to take 
+i = 0 & j = 1 so that on i++, difference decreases and on j++
+difference increases and then handle the cases.
+Follow this in all Two Pointers Questions
 
 int Solution::diffPossible(vector<int> &A, int B) {
     int ans = 0;
