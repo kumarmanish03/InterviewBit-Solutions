95f000ba577ed4ba1a181e80e676c44d97cd1514
Manish Kumar <manish15052000@gmail.com>
2021-05-21 10:30:35 +0530

    two pointer

---
+++ Two Pointers/Counting Subarrays.cpp
@@ -0,0 +1,26 @@
+// Manish Kumar, IIIT Allahabad
+// Medium
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+https://www.interviewbit.com/problems/counting-subarrays/
+
+int Solution::solve(vector<int> &A, int B) {
+    int i = 0;
+    int sum = 0;
+    int count = 0;
+    for(int j = 0; j < A.size(); j++){
+        sum += A[j];
+        
+        while(sum >= B){
+            //if(j > i){
+                sum -= A[i];
+                i++;
+            //}
+        }
+        
+        count += j - i + 1;
+    }
+    
+    return count;
+}
--- Two Pointers/Maximum one after modification.cpp
+++ Two Pointers/Maximum one after modification.cpp
@@ -9,33 +9,26 @@ https://www.interviewbit.com/problems/maximum-ones-after-modification/
 
 int Solution::solve(vector<int> &A, int B) {
     int n = A.size();
-    int zero = 0;
-    int maxone = 1;
-    if(!A[0]) zero++;
-    for(int i = 1; i < n; i++){
-        if(!A[i]) zero++;
-    }
-    
-    if(zero <= B) return n;
-    
-    int k = B;
     int i = 0;
-    int j = 0;
-    int curr = 0;
-    int maxm = INT_MIN;
-    while(1) {
-        if(A[j] == 0){
-            k--;
-            curr++;
-            j++;
-        }
-        else if(A[j] == 1){
-            j++;
-            curr++;
+    int w = 0; // width of maximum subarray
+    int k = 0; //zero count
+    for(int j = 0; j < n; j++){
+        if(A[j] == 0)
+            k++;
+            
+        while(k > B) {
+            if(A[i] == 0)
+                k--;
+            i++;
         }
         
-        if(k == 0){
-            maxm = max(curr, maxm);
+        if(j - i + 1 > w){
+            w = j - i + 1;
         }
+        
     }
+    
+    return w;
+    
 }
+
