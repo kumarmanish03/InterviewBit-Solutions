44f3989223263f752f1a1f8df199f7cabc956ba4
Manish Kumar <manish15052000@gmail.com>
2021-06-11 17:31:12 +0530

    LIC

---
+++ Dynamic Programming/chain of pairs.cpp
@@ -0,0 +1,21 @@
+// Manish Kumar, IIIT Allahabad
+// Easy
+
+https://www.interviewbit.com/problems/chain-of-pairs/
+
+// Similar as Longest Increasing Subsequence (only minor change in if statement)
+
+int Solution::solve(vector<vector<int> > &A) {
+    int n = A.size();
+    int inc[n];
+    
+    for(int i = 0; i < n; i++){
+        inc[i] = 1;
+        for(int j = 0; j < i; j++){
+            if(A[i][0] > A[j][1])
+                inc[i] = max(inc[i], inc[j] + 1);
+        }
+    }
+    
+    return *max_element(inc, inc + n);
+}
