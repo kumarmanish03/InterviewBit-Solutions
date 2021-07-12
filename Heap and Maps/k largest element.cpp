b52470027bed70cbb10eaf88b171e49b3ea92b43
Manish Kumar <manish15052000@gmail.com>
2021-07-12 18:21:01 +0530

    nlogk heap

--- Heap and Maps/k largest element.cpp
+++ Heap and Maps/k largest element.cpp
@@ -1,30 +1,27 @@
 // Manish Kumar, IIIT Allahabad
 // Medium
-// Time Complexity : O(nlogn) [worst case]
+// Time Complexity : O(nlogk) [worst case]
 // Space Complexity : O(n)
 
 https://www.interviewbit.com/problems/k-largest-elements/
 
 // You need not to sort the whole array
-// Just use priority_queue and sttore all elements
-// elements are arranged in decreasing order, hence extract top B elements
 
-// If we want to create a priority queue that supports finding and removing the
-// smallest element, we can do it as follows:
-// priority_queue<int,vector<int>,greater<int>> q;
-
-// In this, Insertion and removal take O(logn) time, and retrieval takes O(1) time.
+// TC: nlogk becoz, n for traversing whole array and for each iteration logk for insertion
 
 vector<int> Solution::solve(vector<int> &A, int B) {
     priority_queue<int> q;
     for(int i = 0; i < A.size(); i++){
         q.push(A[i]);
+        if(q.size() > B)
+            q.pop();
     }
     
-    vector<int> v;
+    vector<int> v(B);
     while(B--){
         v.push_back(q.top());
         q.pop();
     }
+    reverse(v.begin(), v.end());
     return v;
 }
