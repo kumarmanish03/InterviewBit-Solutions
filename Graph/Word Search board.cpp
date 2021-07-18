072ab7194b28a75dfb2664d546dfec63a7ab0ff6
Manish Kumar <manish15052000@gmail.com>
2021-07-18 12:46:08 +0530

    Simple DFS

--- Graph/Word Search board.cpp
+++ Graph/Word Search board.cpp
@@ -13,22 +13,17 @@ bool dfs(int i, int j, int k, vector<string> &A, string &B){
         return true;
     if(i < 0 || i >= n || j < 0 || j >= m || A[i][j] != B[k])
         return false;
-
-    if(dfs(i+1, j, k+1) || dfs(i-1, j, k+1) || dfs(i, j-1, k+1) || dfs(i, j+1, k+1)){
+ 
+    if(dfs(i+1, j, k+1, A, B) || dfs(i-1, j, k+1, A, B) || dfs(i, j-1, k+1, A, B) || dfs(i, j+1, k+1, A, B)){
         return true;
     }
     return false;
 }
-
+ 
 int Solution::exist(vector<string> &A, string B) {
-    A.clear();
-    A = A;
-    B = B;
-    n = A.size();
-    m = A[0].size();
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < A.size(); i++){
         for(int j = 0; j < A[i].size(); j++){
-            if(A[i][j] == B[0] && dfs(i, j, 0)){
+            if(A[i][j] == B[0] && dfs(i, j, 0, A, B)){
                 return true;
             }
         }
