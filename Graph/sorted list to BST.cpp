48f1b9bf24279ffc0139f63d49a989a3b86404e8
Manish Kumar <manish15052000@gmail.com>
2021-07-20 19:27:22 +0530

    slow and fast pointer

--- Graph/sorted list to BST.cpp
+++ Graph/sorted list to BST.cpp
@@ -1,3 +1,43 @@
+// Manish Kumar, IIIT Allahabad
+// Hard
+
+https://www.interviewbit.com/problems/convert-sorted-list-to-binary-search-tree/
+
+// slow and fast pointer technique
+
+ListNode* getMid(ListNode* &head){
+    ListNode *slow = head, *fast = head, *pre = nullptr;
+
+    while(fast and fast->next){
+        pre = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    // separate list from mid
+    if(pre)
+        pre->next = nullptr;
+
+    // this slow ptr points to mid element of the list
+    return slow;
+}
+
+TreeNode* Solution::sortedListToBST(ListNode* A) {
+    if(!A)
+        return nullptr;
+    
+    ListNode* mid = getMid(A);
+    TreeNode* root = new TreeNode(mid->val);
+    // when only one node
+    if(mid == A)
+        return root;
+    root->left = sortedListToBST(A);
+    root->right = sortedListToBST(mid->next);
+
+    return root;
+}
+
+
+
 // Using vectors
 
 void fun(int lo, int hi, vector<int> &v){
