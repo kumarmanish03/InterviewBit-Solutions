3adca73361682f43bec1bc365e0a9b141347a7b0
Manish Kumar <manish15052000@gmail.com>
2021-07-23 11:37:37 +0530

    Easy

--- Stacks & Queues/Redundant Braces.cpp
+++ Stacks & Queues/Redundant Braces.cpp
@@ -6,17 +6,24 @@
 https://www.interviewbit.com/problems/redundant-braces/
 
 int Solution::braces(string A) {
-    int cnt =0;
-    for(auto c : A){
-        if(c == '(') cnt++;
-        if(c == '+' || c == '*' || c == '-' || c == '/') {
-            if(cnt != 0)
-                cnt--;
+    int n = A.size();
+    stack<char> s;
+    for(int i = 0; i < n; i++){
+        if(A[i] == '(' || A[i] == '+' || A[i] == '-' || A[i] == '*' || A[i] == '/'){
+            s.push(A[i]);
         }
-            
+        if(A[i] == ')'){
+            if(s.top() == '('){
+                return 1;
+            }
+            else{
+                while(s.top() == '+' || s.top() == '-' || s.top() == '*' || s.top() == '/'){
+                    s.pop();
+                }
+                s.pop();
+            }
+        }
     }
-    if(cnt > 0)
-        return 1;
-    else
-        return 0;
+    return 0;
 }
+
