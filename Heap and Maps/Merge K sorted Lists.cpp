d8b59ef896a93aa2e5ecff3fcad72f12035beb82
Manish Kumar <manish15052000@gmail.com>
2021-07-25 00:27:42 +0530

    Same as 'Merge K sorted arrays'

--- Heap and Maps/Merge K sorted Lists.cpp
+++ Heap and Maps/Merge K sorted Lists.cpp
@@ -7,6 +7,9 @@
 
 https://www.interviewbit.com/problems/merge-k-sorted-lists/
 
+
+// Soln 1 (can be done without using PP pair, see soln 2)
+
 #define PP pair<int, ListNode*>
 
 ListNode* Solution::mergeKLists(vector<ListNode*> &A) {
@@ -25,6 +28,37 @@ ListNode* Solution::mergeKLists(vector<ListNode*> &A) {
         ans = node;
         if(node->next)
             pq.push({node->next->val, node->next});
+    }
+
+    return res->next;
+}
+
+
+// Soln 2
+// Using comparator function
+
+struct comp{
+    bool operator()(auto &x, auto &y){
+        return x->val > y->val;
+    }    
+};
+
+ListNode* Solution::mergeKLists(vector<ListNode*> &A) {
+    priority_queue<ListNode*, vector<ListNode*>, comp> pq;
+    for(auto node : A){
+        pq.push(node);
+    }
+    // dummy Node
+    ListNode *ans = new ListNode(0);
+    ListNode *res = ans;
+
+    while(!pq.empty()){
+        ListNode *node = pq.top();
+        pq.pop();
+        ans->next = node;
+        ans = node;
+        if(node->next)
+            pq.push(node->next);
     }
 
     return res->next;
