2deba694a6fe3506fd81dc6e39070aa011b6bed2
Manish Kumar <manish15052000@gmail.com>
2021-07-25 00:13:19 +0530

    Same as 'Merge K sorted arrays'

---
+++ Heap and Maps/Merge K sorted Lists.cpp
@@ -0,0 +1,31 @@
+// Manish Kumar, IIIT Allahabad
+// Easy
+// TC : O(n*k*logk)
+// SC : O(n*k)
+
+// Strategy : Same as 'Merge K sorted arrays'
+
+https://www.interviewbit.com/problems/merge-k-sorted-lists/
+
+#define PP pair<int, ListNode*>
+
+ListNode* Solution::mergeKLists(vector<ListNode*> &A) {
+    priority_queue<PP, vector<PP>, greater<PP>> pq;
+    for(auto node : A){
+        pq.push({node->val, node});
+    }
+    // dummy Node
+    ListNode *ans = new ListNode(0);
+    ListNode *res = ans;
+
+    while(!pq.empty()){
+        ListNode *node = pq.top().second;
+        pq.pop();
+        ans->next = node;
+        ans = node;
+        if(node->next)
+            pq.push({node->next->val, node->next});
+    }
+
+    return res->next;
+}
