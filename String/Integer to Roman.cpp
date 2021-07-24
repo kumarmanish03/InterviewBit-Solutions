5cc65c028df77527d6f44b70be9f6f9fdda7ac5b
Manish Kumar <manish15052000@gmail.com>
2021-07-24 15:18:33 +0530

    2 Diff Soln

--- String/Integer to Roman.cpp
+++ String/Integer to Roman.cpp
@@ -3,6 +3,8 @@
 
 https://www.interviewbit.com/problems/integer-to-roman/
 
+// =================================================
+// Soln 1
 // Also see IB soln
 
 string Solution::intToRoman(int A) {
@@ -35,3 +37,22 @@ string Solution::intToRoman(int A) {
     }
     return  res;
 }
+
+
+// =======================================
+// Soln 2
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        // 1000, 2000, 3000
+        string M[] = {"", "M", "MM", "MMM"};
+        // 100, 200, 300, .. 900
+        string C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        // 10, 20, ... 90
+        string X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        // 1, 2, ... 9
+        string I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
+};
