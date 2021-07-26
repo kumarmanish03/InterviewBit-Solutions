fc2e7bb91d6f0721525c38297f70ef8ac8d5a779
Manish Kumar <manish15052000@gmail.com>
2021-07-26 14:12:19 +0530

    Using maxQueue

--- Stacks & Queues/Sliding Window maximum.cpp
+++ Stacks & Queues/Sliding Window maximum.cpp
@@ -5,6 +5,56 @@
 
 https://www.interviewbit.com/problems/sliding-window-maximum/
 
+// Soln 1
+// Deque s.t. it always stores Maximum in front
+deque<int> dq;
+
+int getMax(){
+    return dq.front();
+}
+
+void push(int element){
+    while(!dq.empty() && dq.back() < element)
+        dq.pop_back();
+    dq.push_back(element);
+}
+
+void pop(int element){
+    if(!dq.empty() && element == dq.front()){
+        dq.pop_front();
+    }
+}
+
+vector<int> Solution::slidingMaximum(const vector<int> &A, int B) {
+    int n = A.size();
+    dq.clear();
+    vector<int> v;
+    if(B >= n){
+        return {*max_element(A.begin(), A.end())};
+    }
+
+    for(int i = 0; i < B; i++){
+        push(A[i]);
+    }
+    v.push_back(getMax());
+
+    for(int i = B; i < n; i++){
+        // Pushing current element
+        push(A[i]);
+        
+        // Poping last max_element
+        pop(A[i-B]);
+        
+        v.push_back(getMax());
+    }
+    
+    return v;
+}
+
+
+// ==========================================
+// Soln 2
+
 https://www.youtube.com/watch?v=39grPZtywyQ
 
 // Watch video for Explanation
