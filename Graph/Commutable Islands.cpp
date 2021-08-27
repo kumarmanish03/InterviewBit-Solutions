fa4d2e817779edd70dd217361068cadd03f066be
Manish Kumar <manish15052000@gmail.com>
2021-08-27 17:58:41 +0530

    prims and kruskal algo

--- Graph/Commutable Islands.cpp
+++ Graph/Commutable Islands.cpp
@@ -2,6 +2,7 @@
 // Medum
 
 // Prims Algorithm
+// TC : O()
 
 https://www.interviewbit.com/old/problems/commutable-islands/
 
@@ -28,5 +29,45 @@ int Solution::solve(int A, vector<vector<int> > &B) {
             }
         }
     }
+    return minCost;
+}
+
+
+// Kruskals Algorithm
+// TC : O(ElogE + EV)
+
+bool comp(vector<int> &a, vector<int> &b){
+    return a[2] < b[2];
+}
+
+int findP(int u, vector<int> &dsuf){
+    if(dsuf[u] == -1){
+        return u;
+    }
+    return findP(dsuf[u], dsuf);
+}
+
+int Solution::solve(int A, vector<vector<int> > &B) {
+    vector<int> dsuf(A+1, -1);
+    
+    sort(B.begin(), B.end(), comp);
+    
+    int minCost = 0, cnt = 0, i = 0;
+    while(cnt != A-1){
+        vector<int> u = B[i];
+        int fromP = findP(u[0], dsuf);
+        int toP = findP(u[1], dsuf);
+        
+        if(fromP == toP){
+            i++;
+            continue;
+        }
+        
+        dsuf[fromP] = toP;
+        minCost += u[2];
+        i++;
+        cnt++;
+    }
+    
     return minCost;
 }
