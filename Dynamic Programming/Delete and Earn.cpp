1c422236ad72e05c74f177999cefdb8458245656
Manish Kumar <41566808+kumarmanish03@users.noreply.github.com>
2022-01-28 23:40:06 +0530

    Same as: Find max sum such that no two consecutive added

---
+++ Dynamic Programming/Delete and Earn.cpp
@@ -0,0 +1,23 @@
+// Manish Kumar, IIIT Allahabad
+
+// If you clearly observe this problem reduce to-
+// "Find max sum such that no two consecutive added" OR Stlicker thieve problem GFG
+
+class Solution {
+public:
+    
+    int deleteAndEarn(vector<int>& num) {
+        int mx = *max_element(num.begin(), num.end());
+        vector<int> a(mx+1, 0);
+        for(int i : num){
+            a[i]+=i;
+        }
+        int dp[mx+1];
+        dp[0] = a[0];
+        dp[1] = max(a[1], a[0]);
+        for(int i = 2; i < mx+1; i++){
+            dp[i] = max(dp[i-1], dp[i-2] + a[i]);
+        }
+        return dp[mx];
+    }
+};
